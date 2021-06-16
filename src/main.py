c974faa18510a3b5443f11404fac8ec7d44ab9e8
Adam Szalowski <adamszalowski99@gmail.com>
2021-06-16 23:44:07 +0200

    Add tests

--- src/main.py
+++ src/main.py
@@ -47,7 +47,7 @@ def compare_local_global():
 
 def compare_crossover():
     history = []
-    for i in range(5):
+    for i in range(10):
         filename = '../data/winequality-white.csv'
         X, y = processWineData(filename)
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
@@ -78,35 +78,67 @@ def compare_crossover():
         print(i)
         print()
 
+def compare_mutation():
+    history = []
+    for i in range(10):
+        filename = '../data/winequality-white.csv'
+        X, y = processWineData(filename)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+        xgb_tuner = XGBoostTuner(X_train, y_train, variation='global', mutation_prob=0.2)
+        xgb_tuner.run(10)
+        params = xgb_tuner.getBestParams()
+
+        xg_clas = xgb.XGBClassifier(**params, use_label_encoder=False)
+        xg_clas.fit(X_train, y_train, eval_metric = "mlogloss")                   
+        preds = xg_clas.predict(X_test)
+        fitness_score_02 = f1_score(y_test, preds)
+
+        xgb_tuner = XGBoostTuner(X_train, y_train, variation='global', mutation_prob=0.7)
+        xgb_tuner.run(10)
+        params = xgb_tuner.getBestParams()
+
+        xg_clas = xgb.XGBClassifier(**params, use_label_encoder=False)
+        xg_clas.fit(X_train, y_train, eval_metric = "mlogloss")                   
+        preds = xg_clas.predict(X_test)
+        fitness_score_07 = f1_score(y_test, preds)
+
+
+        history.append((i, fitness_score_02, fitness_score_07))
+        df = pd.DataFrame(history)  
+        df.to_excel("compare_mut.xlsx")
+
+        print(i)
+        print()
+
 
 if __name__ == "__main__":
-    # filename = '../data/winequality-white.csv'
-    # X, y = processWineData(filename)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+    filename = '../data/winequality-white.csv'
+    X, y = processWineData(filename)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
-    # xg_clas = xgb.XGBClassifier(use_label_encoder=False)
-    # xg_clas.fit(X_train, y_train, eval_metric = "mlogloss")                   
-    # preds = xg_clas.predict(X_test)
-    # fitness_score = f1_score(y_test, preds)
-    # accuracy = accuracy_score(y_test, preds)
+    xg_clas = xgb.XGBClassifier(use_label_encoder=False)
+    xg_clas.fit(X_train, y_train, eval_metric = "mlogloss")                   
+    preds = xg_clas.predict(X_test)
+    fitness_score = f1_score(y_test, preds)
+    accuracy = accuracy_score(y_test, preds)
     
-    # print(f"Base fitness score: {fitness_score}")
-    # print(f"Base accuracy score: {accuracy}")
+    print(f"Base fitness score: {fitness_score}")
+    print(f"Base accuracy score: {accuracy}")
 
-    # xgb_tuner = XGBoostTuner(X_train, y_train)
-    # xgb_tuner.run()
-    # params = xgb_tuner.getBestParams()
+    xgb_tuner = XGBoostTuner(X_train, y_train)
+    xgb_tuner.run()
+    params = xgb_tuner.getBestParams()
 
-    # xg_clas = xgb.XGBClassifier(**params, use_label_encoder=False)
-    # xg_clas.fit(X_train, y_train, eval_metric = "mlogloss")                   
-    # preds = xg_clas.predict(X_test)
-    # fitness_score = f1_score(y_test, preds)
-    # accuracy = accuracy_score(y_test, preds)
+    xg_clas = xgb.XGBClassifier(**params, use_label_encoder=False)
+    xg_clas.fit(X_train, y_train, eval_metric = "mlogloss")                   
+    preds = xg_clas.predict(X_test)
+    fitness_score = f1_score(y_test, preds)
+    accuracy = accuracy_score(y_test, preds)
     
-    # print(f"Base fitness score: {fitness_score}")
-    # print(f"Base accuracy score: {accuracy}")
+    print(f"Base fitness score: {fitness_score}")
+    print(f"Base accuracy score: {accuracy}")
 
-    compare_crossover()
 
 
 
